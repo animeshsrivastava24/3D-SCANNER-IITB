a9db911207ddad08df95a14fb1a4194dc346f345
Soumya Sambit Rath <rath.soumyasambit@gmail.com>
2017-06-08 23:15:56 +0530

    Prerequisites
    Includes all the installation commands of the dependencies and the packages

---
+++ installscript.sh
@@ -0,0 +1,62 @@
+#'''This code is a part of the 3D Scanner project'''
+#'''Developed by team SAAS'''
+#'''Ekalavya 2017'''
+#'''IIT Bombay'''
+#INSTALLATION BASH FILE FOR THE REQUIREMENTS OF 3D SCANNER PROJECT
+
+# 1. KEEP UBUNTU OR DEBIAN UP TO DATE
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get -y autoremove
+
+
+# 2. INSTALL THE DEPENDENCIES
+
+# Build tools:
+sudo apt-get install -y build-essential cmake
+
+# GUI (if you want to use GTK instead of Qt, replace 'qt5-default' with 'libgtkglext1-dev' and remove '-DWITH_QT=ON' option in CMake):
+sudo apt-get install -y qt5-default libvtk6-dev
+
+# Media I/O:
+sudo apt-get install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libjasper-dev libopenexr-dev libgdal-dev
+
+# Video I/O:
+sudo apt-get install -y libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev
+
+# Parallelism and linear algebra libraries:
+sudo apt-get install -y libtbb-dev libeigen3-dev
+
+# Python:
+sudo apt-get install -y python-dev python-tk python-numpy python3-dev python3-tk python3-numpy
+
+# PIP:
+sudo apt-get -y install python-pip
+sudo pip install matplotlib
+sudo pip install PyOpenGL
+sudo pip install v4l2
+sudo pip install imutils
+sudo pip install pygame
+sudo pip install trimesh
+
+
+# GLUT:
+sudo apt-get install freeglut3-dev
+
+# 3. INSTALL THE LIBRARY (YOU CAN CHANGE '3.2.0' FOR THE LAST STABLE VERSION)
+
+sudo apt-get install -y unzip wget
+wget https://github.com/opencv/opencv/archive/3.2.0.zip
+unzip 3.2.0.zip
+rm 3.2.0.zip
+mv opencv-3.2.0 OpenCV
+cd OpenCV
+mkdir build
+cd build
+cmake -DWITH_QT=ON -DWITH_OPENGL=ON -DFORCE_VTK=ON -DWITH_TBB=ON -DWITH_GDAL=ON -DWITH_XINE=ON -DBUILD_EXAMPLES=ON ..
+make -j4
+sudo make install
+sudo ldconfig
+
