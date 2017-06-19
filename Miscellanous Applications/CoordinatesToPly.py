636a90e8850fc126264d750c3f87f3ed4dcd38d4
Anchal Singh <anchalpanwar551996@gmail.com>
2017-06-19 11:39:16 +0530

    added element face

--- Miscellanous Applications/CoordinatesToPly.py
+++ Miscellanous Applications/CoordinatesToPly.py
@@ -1,7 +1,7 @@
 import numpy as np
 f=open("coord.ply","w+")
 s=[]
-for i in range(7):
+for i in range(8):
 	s.append("0")
 X=[1,2,3,4]
 Y=[5,3,9,6]
@@ -12,7 +12,8 @@ s[2]="element vertex "+ str(len(X))
 s[3]="property float32 x"
 s[4]="property float32 y"
 s[5]="property float32 z"
-s[6]="end_header"
+s[6]="element face 0"
+s[7]="end_header"
 
 #Copying the format of a .ply file into "coord.ply" file
 for i in range(len(s)):
