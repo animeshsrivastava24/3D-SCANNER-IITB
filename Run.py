d49f35b482bd8b37fb62bd495194a12306c6aea4
ani <animeshsri.nith@gmail.com>
2017-07-04 01:21:10 +0530

    Update Run.py

--- Run.py
+++ Run.py
@@ -8,10 +8,12 @@
 import Main #local package
 import threading #local package
 
+
 th1=threading.Thread(target=Main.Main()) #Main() defined inside Main.py is called inside a thread
 th1.start() #The thread is started
 
 '''PS:
 The Developers have used threading to start processes from the very beginning of the program.
 This was done to enable execution of any other process parallely with the main process during any point of the runtime.'''
+
 
